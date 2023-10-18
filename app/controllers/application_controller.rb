af967604c3d18f23c9842942ba47e47d98c6c1f4
mahfuz <mahfuzflamedestiny@gmail.com>
2023-10-18 12:42:42 +0530

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(1)
   end
 
   def authenticate_user
